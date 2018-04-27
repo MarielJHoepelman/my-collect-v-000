03ccf7ccaf48adcea0519a3ccc227843c2d1f49f
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-04-27 01:22:03 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,34 +1,42 @@
-# def my_collect(array)
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+# def upcase_dindos(array)
 #   i = 0
 #   collection = []
 #   while i < array.length
-#     collection << yield(array[i])
+#     collection << "I love #{array[i].upcase}!"
+#     i += 1
+#   end
+#   collection
+# end
+#
+# def reverse_dindos(array)
+#   i = 0
+#   collection = []
+#   while i < array.length
+#     collection << "I love #{array[i].reverse}!"
 #     i += 1
 #   end
 #   collection
 # end
 
-def upcase_dindos(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << "I love #{array[i].upcase}!"
-    i += 1
-  end
-  collection
-end
-
-def reverse_dindos(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << "I love #{array[i].reverse}!"
-    i += 1
-  end
-  collection
-end
-
 dindos = ["mush", "quatar", "gana"]
 
-puts upcase_dindos(dindos)
-puts reverse_dindos(dindos)
+# puts upcase_dindos(dindos)
+# puts reverse_dindos(dindos)
+
+upcase = my_collect(dindos) do |dindo|
+  "I love #{array[i].upcase}!"
+end
+
+reverse = my_collect(dindos) do |dindo|
+  "I love #{array[i].reverse}!"
+end
