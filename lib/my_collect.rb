5a85be139c68dbad7d03eb9b26a2ae3cb2f47b31
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-04-27 01:19:09 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
-def my_collect(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
+# def my_collect(array)
+#   i = 0
+#   collection = []
+#   while i < array.length
+#     collection << yield(array[i])
+#     i += 1
+#   end
+#   collection
+# end
 
 def upcase_dindos(array)
   i = 0
